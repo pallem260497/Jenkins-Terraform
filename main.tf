76250adae5941eb62303a21baed1a8960e8de9e8
pallem260497 <99157306+pallem260497@users.noreply.github.com>
2022-03-21 10:35:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,14 @@
 resource "aws_instance" "testinstance" {
 
-ami = "ami — 028598a84ca601344"
+    ami = "ami-07e19c485c7cf2266"
+    instance_type = "t2.micro"
+    associate_public_ip_address = true
+    key_name = "DonotDeleteKeyPair"
 
-instance_type = "t2.micro"
+    tags {
 
-associate_public_ip_address = true
+      Name = “testinstance”
 
-key_name = "DonotDeleteKeyPair"
-
-tags {
-
-  Name = “testinstance”
-
-}
+    }
 
 }
